f4eb7c588da05c4829706742bc738b31278b222f
shivnaytiwari <shivnaytiwari@gmail.com>
2018-01-01 23:50:23 +0530

    fsgfdg

---
+++ app/src/main/java/com/example/sandeep/toastexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sandeep.toastexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"your Message to toast",Toast.LENGTH_LONG).show();
+
+    }
+}
